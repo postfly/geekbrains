0fcdc4ac1f9a32d5c2c6e40894755f20c287ec5b
postfly <postfly@mail.ru>
2020-11-14 19:40:06 +0300

    [upd] Исправлена задача 3.2 в соответствии с разбором домашней работы на видео

--- python/lesson_3/hw_2.py
+++ python/lesson_3/hw_2.py
@@ -1,14 +1,14 @@
-def user_data(**kwargs):
-    return kwargs
+def user_data(name, surname, year_birth, city, email, phone):
+    return f'Name: {name}\n' \
+           f'Surname: {surname}\n' \
+           f'Year birth: {year_birth}\n' \
+           f'City: {city}\n' \
+           f'Email: {email}\n' \
+           f'Phone: {phone}\n'
 
-
-name = input('Укажите имя: ')
-surname = input('Укажите фамилию: ')
-year_birth = int(input('Укажите год рождения: '))
-city_residence = input('Укажите город проживания: ')
-email = input('Укажите email: ')
-phone = int(input('Укажите номер телефона: '))
-
-result = user_data(Имя=name, Фамилия=surname, Год_рождения=year_birth, Город_проживания=city_residence, Email=email,
-                   Телефон=phone)
-print(result)
+print(user_data(name='Vadim',
+                surname='Besputin',
+                year_birth='11.06.1981',
+                city='Moscow',
+                email='postfly@mail.ru',
+                phone='+79000000000'))
