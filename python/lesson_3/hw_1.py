d8b591e6a44225f4b08e5abb66fdd875cb8ee7de
postfly <postfly@mail.ru>
2020-11-14 18:59:01 +0300

    [upd] Исправлена задача 3.1. Решение с обработкой ошибки ZeroDivisionError

--- python/lesson_3/hw_1.py
+++ python/lesson_3/hw_1.py
@@ -1,9 +1,9 @@
 def division(arg_1, arg_2):
-    if arg_2 == 0:
-        return 'На ноль делить нельзя'
-    s_div = arg_1 / arg_2
-    return s_div
-
+    try:
+        s_div = arg_1 / arg_2
+        return s_div
+    except ZeroDivisionError as err:
+        return print('Деление на ноль', err)
 
 num_1 = float(input('Введите первое число: '))
 num_2 = float(input('Введите второе число: '))
