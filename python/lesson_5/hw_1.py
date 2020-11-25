68e3faa7297db008e7ad4c56e30f14642cf2f21f
postfly <72794632+postfly@users.noreply.github.com>
2020-11-25 15:31:28 +0300

    Update hw_1.py
    Обновил условие согласно рекомендации.

--- python/lesson_5/hw_1.py
+++ python/lesson_5/hw_1.py
@@ -1,7 +1,7 @@
 with open('text.txt', 'a') as f_obj:
     while True:
         line = input('Some text. Press enter to finish: ')
-        if line != "":
+        if line:
             f_obj.write(f"{line}\n")
         else:
             break
